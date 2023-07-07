faf056a569b3d57dfd2c9cbd77d63a78ef93dcfb
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-07-07 13:32:10 -0500

    Initial Commit and set up

---
+++ src/pages/_app.js
@@ -0,0 +1,27 @@
+import '@/styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+
+export default function App({ Component, pageProps }) {
+
+  const colors ={
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac'
+    },
+  }
+
+  const theme = extendTheme({ colors })
+  
+  return (
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+   
+  )
+}
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
